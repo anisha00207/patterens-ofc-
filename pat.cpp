7bbeac3a7ed88f0864927dea4716287bb130db29
anisha00207 <90251007+anisha00207@users.noreply.github.com>
2023-01-12 11:03:52 +0530

    Create pat.cpp

---
+++ pat.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i;int n;int a;
+cin>>n;
+    for(i=1;i<=n;i++){
+        
+        if(i-1==0)
+    {for(int l=1;l<=n;l++)
+        cout<<"# ";
+    }
+
+    
+        else{
+           int a=i-1;
+            for(int j=1;j<=a;j++)
+            {
+                cout<<"  ";
+            }
+            for(int k=i;k<=n;k++){
+                cout<<"# ";
+            }
+            
+            }
+    cout<<endl;
+        
+    }
+    
+    return 0;
+    
+}
+
+/*
+# # # # #
+* # # # #
+* * # # #
+* * * # #
+* * * * #
+
+
+*/
